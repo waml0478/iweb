d85e0e66155e82558f77abe3d94fe66fa2050b39
waml0478 <wamlvzla@gmail.com>
2016-09-11 22:35:49 -0400

    quinto repositorio fin de semana

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var jade = require('gulp-jade');
+var imagesop = require('gulp-image-optimization');
 var navegador = require('browser-sync');
 
 // MOVER
 gulp.task('mover', function(){
   //gulp.src('./bower_components/jade-bootstrap/css/*.css').pipe(gulp.dest('./dist/css'));
-  gulp.src('./bower_components/bootstrap/dist/css/bootstrap.min.css').pipe(gulp.dest('./dist/css'));  
+  gulp.src('./bower_components/bootstrap/dist/css/bootstrap.min.css').pipe(gulp.dest('./dist/css'));
   gulp.src('./bower_components/bootstrap/dist/js/bootstrap.min.js').pipe(gulp.dest('./dist/js'));
   gulp.src('./bower_components/jquery/dist/jquery.min.js').pipe(gulp.dest('./dist/js'));
   gulp.src('./src/img/*.*').pipe(gulp.dest('./dist/img'));
@@ -30,6 +31,18 @@ gulp.task('jade',function(){
   .pipe(gulp.dest('./dist/'))
   });
 
+  // IMAGE-OPTIMIZATION
+  //gulp.src(['./src/img/*.jpg','./src/img/*.jpeg','./src/img/*.png','./src/img/*.gif'])
+  gulp.task('imagesop', function(){
+  	gulp.src('./src/img/*.*')
+  	.pipe(imagesop({
+      optimizationLevel: 5,
+      progressive: true,
+      interlaced: true
+    }))
+  	.pipe(gulp.dest('./dist/img/'));
+  	});
+
 //SERVIDOR
   gulp.task('server', function (){
   	 var files = [
@@ -48,6 +61,7 @@ gulp.task('jade',function(){
 //DEFAULT
   gulp.task('default',['server'],function(){
   	gulp.watch('./src/sass/*.sass',['sass']);
+    gulp.watch('./src/img/*.*',['imagesop']);
     gulp.watch('./src/img/*.*',['mover']);
     gulp.watch('./src/layouts/*.jade',['jade']);
   });
